f7fa95c6b7f2a57af87a57188128781dcfa226ce
Big W <wendyuworks@gmail.com>
2020-07-20 11:06:00 +1000

    add homepage

---
+++ techmeet-client/src/pages/homePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const homePage = () => {
+
+    return (
+        <>
+        <div> Hello World </div>
+        <div> Hello again?</div>
+        </>
+    )
+}
+
+
+export default homePage;
