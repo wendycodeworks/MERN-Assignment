cded78d07eebfe86fc1b10566193ff06c5e68635
Big W <wendyuworks@gmail.com>
2020-07-22 15:59:22 +1000

    create event model

---
+++ backend/src/models/event.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    banner: {
+        
+    },
+
+})
+
+module.exports = mongoose.model("Event", eventSchema);
