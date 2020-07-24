7060cccc4cb53fac9191c90a546d461ba72466b8
Big W <wendyuworks@gmail.com>
2020-07-24 20:42:05 +1000

    init commit

--- techmeet-client/src/App.js
+++ techmeet-client/src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 import HomePage from "./pages/HomePage.js";
+import AddEvent from "./pages/Event/AddEvent.js"
+import {BrowserRouter, Route} from "react-router-dom"
 
-const App = () => {
-  return (
-    <>
-    <HomePage />
-    </>
-  );
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <BrowserRouter>
+        <div>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/new" component={AddEvent} />
+        </div>
+        </BrowserRouter>
+      </div>
+    )
+  }
 }
 
 export default App;
