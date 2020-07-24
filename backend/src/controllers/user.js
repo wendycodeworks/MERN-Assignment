1acafbe34d8dae9e5ec2205e2c3f8dac225afe88
Morgan Webb <mnbwebb2233@gmail.com>
2020-07-24 10:26:25 +1000

    handle route to show all users and destroy

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -1,14 +1,16 @@
 const Logger = require("../helpers/logger");
 const User = require("../models/user");
 
-const index = (req, res) => {
-  req.session.views = req.session.views ? req.session.views + 1 : 1;
-  res
-    .status(200)
-    .send(JSON.stringify({ status: "OK", views: req.session.views }));
+const index = async (req, res) => {
+  // empty filter to return all users.
+  const users = await User.find();
+  res.status(200).send(JSON.stringify(users));
 };
 
-const show = (req, res) => {};
+const show = async (req, res) => {
+  const user = await User.findById(req.params.id);
+  res.status(200).send(JSON.stringify(user));
+};
 
 const create = async (req, res) => {
   const { firstName, lastName, email, phoneNumber, password } = req.body;
@@ -48,8 +50,26 @@ const create = async (req, res) => {
     });
 };
 
+const destroy = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).send(
+      JSON.stringify({
+        status: "OK",
+      })
+    );
+  } catch (err) {
+    res.status(400).send(
+      JSON.stringify({
+        error: `Could not find user with id: ${req.params.id}`,
+      })
+    );
+  }
+};
+
 module.exports = {
   index: index,
   show: show,
   create: create,
+  destroy: destroy,
 };
--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const userController = require("../controllers/user");
 
 router.get("/", userController.index);
+router.get("/:id", userController.show);
 router.post("/", userController.create);
+router.delete("/:id", userController.destroy);
 
 module.exports = router;
