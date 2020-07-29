d8eb2dacf8f0a3b648dde7b7440269849edaa7ca
Big W <wendyuworks@gmail.com>
2020-07-29 12:49:29 +1000

    add user context to app.js

--- techmeet-client/src/App.js
+++ techmeet-client/src/App.js
@@ -3,24 +3,28 @@ import HomePage from "./pages/HomePage";
 import NavBar from "./pages/components/NavBar"
 import AddEvent from "./pages/Event/AddEvent";
 import ViewEvent from "./pages/Event/ViewEvent";
-import {BrowserRouter, Route, Link, Switch} from "react-router-dom"
+import {BrowserRouter as Router, Route, Link, Switch} from "react-router-dom"
 import Login from "./pages/User/Login.js"
 import Signup from "./pages/User/Signup.js"
+import UserContext from "./pages/_utils/UserContext"
 
 class App extends Component {
   render(){
     return(
       <div>
-    <BrowserRouter>
-      <NavBar />
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/new" component={AddEvent} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/event" component={ViewEvent}/>
-            <Route exact path="/signup" component={Signup}/>
-        </Switch>
-     </BrowserRouter>
+    <Router>
+    
+      <UserContext.Provider value="Hello I'm Context">
+        <NavBar />
+          <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/new" component={AddEvent} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/event" component={ViewEvent}/>
+              <Route exact path="/signup" component={Signup}/>
+          </Switch>
+        </UserContext.Provider>
+     </Router>
       </div>
     )
   }
