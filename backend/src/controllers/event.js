17e5fd1fbee96b3fd199a3f93f68c15a76b1e892
Morgan Webb <mnbwebb2233@gmail.com>
2020-07-29 18:10:11 +1000

    add event attendee route

--- backend/src/controllers/event.js
+++ backend/src/controllers/event.js
@@ -97,10 +97,27 @@ const destroy = async (req, res) => {
 
 }
 
+const addAttendee = (req, res) => {
+    try {
+        const event = await Event.findById(req.body._id);
+        let attendees = event.attendees;
+        attendees.push(req.body.attendee);
+        event.update({ _id: req.body._id }, {
+            attendees: attendees
+        });
+        new Logger("mongoose", `Added attendee with id: ${req.body.attendee} to ${req.body._id}`);
+        res.status(200).send(JSON.stringify({ status: "OK" }));
+    } catch (err) {
+        new Logger("mongoose", `Couldn't find event to add attendee to with id: ${req.body._id}`);
+        res.status(400).send(JSON.stringify({ status: "ERR NOT FOUND" }));
+    }
+}
+
 module.exports = {
   index: index,
   show: show,
   create: create,
   update: update,
-  destroy: destroy
+  destroy: destroy,
+  addAttendee: addAttendee
 }
--- backend/src/routes/event.js
+++ backend/src/routes/event.js
@@ -9,5 +9,6 @@ router.get("/:id", eventController.show);
 router.post("/", passport.authenticate('jwt', { session: false }), upload.single("image"), eventController.create);
 router.put("/:id", passport.authenticate('jwt', { session: false }), eventController.update);
 router.delete("/:id", passport.authenticate('jwt', { session: false }), eventController.destroy);
+router.post("/:id/attendee", passport.authenticate('jwt', { session: false }), eventController.addAttendee);
 
 module.exports = router;
