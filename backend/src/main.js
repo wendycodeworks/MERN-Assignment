45f2a855485623fab6849bf6e6769fc049128497
Morgan Webb <mnbwebb2233@gmail.com>
2020-07-30 19:08:00 +1000

    cors update

--- backend/src/main.js
+++ backend/src/main.js
@@ -36,24 +36,10 @@ app.use(expressSession({
   store: new MongoStore({ mongooseConnection: mongoose.connection })
 }));
 
-// because things broke last minute...
-// https://stackoverflow.com/questions/18310394/no-access-control-allow-origin-node-apache-port-issue
 app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', '*');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', false);
-
-    // Pass to next layer of middleware
     next();
 });
 
